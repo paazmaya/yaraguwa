98be4bd1941e749f9cd1997a096c6c8a645f125c
Juga Paazmaya <paazmaya@yahoo.com>
2016-08-21 13:34:19 +0300

    This must be the 1st :princess: to go along with :pig:

---
+++ README.md
@@ -0,0 +1,7 @@
+# yaraguwa (屋良小)
+
+> Check the health of GitHub repositories and visualise it with D3.js
+
+List all open issues and pull requests in the given users repositories
+
+https://developer.github.com/v3/pulls/#list-pull-requests
---
+++ index.js
@@ -0,0 +1,10 @@
+/**
+ * yaraguwa (屋良小)
+ * https://github.com/paazmaya/yaraguwa
+ *
+ * Check the health of GitHub repositories and visualise it with D3.js
+ *
+ * Copyright (c) Juga Paazmaya <paazmaya@yahoo.com> (https://paazmaya.fi)
+ * Licensed under the MIT license
+ */
+'use strict';
